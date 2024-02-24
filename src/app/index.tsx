bb48838d0d9c0ec471a87616b1600e5aeb089564
na-go <mitsume.anko@gmail.com>
2024-02-24 23:11:37 +0900

    add vanilla extract

---
+++ src/app/index.tsx
@@ -0,0 +1,15 @@
+import { memo } from 'react';
+
+import * as styles from './styles.css';
+
+const App = () => {
+  return (
+    <div className={styles.root}>
+      <main className={styles.main}>
+        <h1 className={styles.title}>Welcome to Vite App.</h1>
+      </main>
+    </div>
+  )
+}
+
+export default memo(App);
---
+++ src/app/styles.css.ts
@@ -0,0 +1,8 @@
+import { style } from "@vanilla-extract/css";
+
+export const root = style({});
+
+export const main = style({
+});
+
+export const title = style({});
