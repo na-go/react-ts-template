e087576d01fae791e5ce079f7f05ba68defb3cb9
na-go <mitsume.anko@gmail.com>
2024-02-25 16:22:02 +0900

    add vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin'
 import tsconfigPaths from 'vite-tsconfig-paths'
@@ -6,4 +6,8 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), vanillaExtractPlugin(), tsconfigPaths()],
+  test: {
+    globals: true,
+    include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}']
+  }
 })
